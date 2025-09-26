8fdb661f9a9468e50b56cae72036c751b8662d45
ArpitaAmbalkar <arpitaambalkar26@gmail.com>
2025-09-26 19:54:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let circle=document.querySelectorAll(".circle");
+circle[0].classList.add("active")
+let line=document.querySelectorAll(".line");
+line[0].classList.add("active-line")
